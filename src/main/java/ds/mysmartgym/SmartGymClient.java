be65054e4a86dc48a40f791cfa800746de3e94b4
Iryna Slynko <iryna.slynko@gmail.com>
2021-07-31 08:52:41 +0100

    Add simple client

---
+++ src/main/java/ds/mysmartgym/SmartGymClient.java
@@ -0,0 +1,56 @@
+package ds.mysmartgym;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.protobuf.Message;
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SmartGymClient {
+    private static final Logger logger = Logger.getLogger(SmartGymClient.class.getName());
+
+    private final MySmartGymGrpc.MySmartGymBlockingStub blockingStub;
+    private final MySmartGymGrpc.MySmartGymStub asyncStub;
+
+  
+    /** Construct client for accessing RouteGuide server using the existing channel. */
+    public SmartGymClient(Channel channel) {
+      blockingStub = MySmartGymGrpc.newBlockingStub(channel);
+      asyncStub = MySmartGymGrpc.newStub(channel);
+    }
+  
+  
+    /** Issues several different requests and then exits. */
+    public static void main(String[] args) throws InterruptedException {
+      String target = "localhost:50051";
+      if (args.length > 0) {
+        if ("--help".equals(args[0])) {
+          System.err.println("Usage: [target]");
+          System.err.println("");
+          System.err.println("  target  The server to connect to. Defaults to " + target);
+          System.exit(1);
+        }
+        target = args[0];
+      }
+      ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+      try {
+        SmartGymClient client = new SmartGymClient(channel);
+        
+        
+      } finally {
+        channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+      }
+    }
+ 
+}

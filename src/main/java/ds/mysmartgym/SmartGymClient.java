a2e37d2a76708659f023b9845871906f69c102dc
Iryna Slynko <iryna.slynko@gmail.com>
2021-08-02 02:32:19 +0100

    Make code run successfully

--- src/main/java/ds/mysmartgym/SmartGymClient.java
+++ src/main/java/ds/mysmartgym/SmartGymClient.java
@@ -5,7 +5,7 @@ import com.google.protobuf.Message;
 import com.google.protobuf.Timestamp;
 import com.google.type.Date;
 import com.google.type.DateTime;
-
+import ds.mysmartgym.*;
 import io.grpc.Channel;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -22,64 +22,70 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import ds.mysmartgym.*;
 
 public class SmartGymClient {
-    private static final Logger logger = Logger.getLogger(SmartGymClient.class.getName());
+  private static final Logger logger =
+      Logger.getLogger(SmartGymClient.class.getName());
 
-    private final MySmartGymGrpc.MySmartGymBlockingStub blockingStub;
-    private final MySmartGymGrpc.MySmartGymStub asyncStub;
+  private final MySmartGymGrpc.MySmartGymBlockingStub blockingStub;
+  private final MySmartGymGrpc.MySmartGymStub asyncStub;
 
-  
-    /** Construct client for accessing RouteGuide server using the existing channel. */
-    public SmartGymClient(Channel channel) {
-      blockingStub = MySmartGymGrpc.newBlockingStub(channel);
-      asyncStub = MySmartGymGrpc.newStub(channel);
-    }
+  /**
+   * Construct client for accessing RouteGuide server using the existing
+   * channel.
+   */
+  public SmartGymClient(Channel channel) {
+    blockingStub = MySmartGymGrpc.newBlockingStub(channel);
+    asyncStub = MySmartGymGrpc.newStub(channel);
+  }
 
-    public void addWeight(float weight) {
-      info("*** Send weight: {0}", weight);
+  public void addWeight(float weight) {
+    info("*** Send weight: {0}", weight);
 
-      Weight request = Weight.newBuilder().setWeight(weight).build();
-      blockingStub.weightUpdate(request);
-    }
+    Weight request = Weight.newBuilder().setWeight(weight).build();
+    blockingStub.weightUpdate(request);
+  }
 
-    public void getWeights() {
-      Empty request = Empty.getDefaultInstance();
-      blockingStub.getSavedWeights(request).forEachRemaining(savedWeight -> {
+  public void getWeights() {
+    Empty request = Empty.getDefaultInstance();
+    info("*** Getting weights");
+    blockingStub.getSavedWeights(request).forEachRemaining(savedWeight -> {
+      info("Got {0} on {1}", savedWeight.getWeight(),
+           fromCustomTime(savedWeight.getTime()));
+    });
+  }
 
-        info("Got {0} on {1}", savedWeight.getWeight(), fromCustomTime(savedWeight.getTime()));
-      });
-      
-    }
-
-    /** Issues several different requests and then exits. */
-    public static void main(String[] args) throws InterruptedException {
-      String target = "localhost:50051";
-      if (args.length > 0) {
-        if ("--help".equals(args[0])) {
-          System.err.println("Usage: [target]");
-          System.err.println("");
-          System.err.println("  target  The server to connect to. Defaults to " + target);
-          System.exit(1);
-        }
-        target = args[0];
+  /** Issues several different requests and then exits. */
+  public static void main(String[] args) throws InterruptedException {
+    String target = "localhost:50051";
+    if (args.length > 0) {
+      if ("--help".equals(args[0])) {
+        System.err.println("Usage: [target]");
+        System.err.println("");
+        System.err.println("  target  The server to connect to. Defaults to " +
+                           target);
+        System.exit(1);
       }
-      ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
-      try {
-        SmartGymClient client = new SmartGymClient(channel);
-        client.addWeight(58);
-        
-      } finally {
-        channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
-      }
+      target = args[0];
     }
+    ManagedChannel channel =
+        ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+    try {
+      SmartGymClient client = new SmartGymClient(channel);
+      client.addWeight(58);
+      client.getWeights();
+    } catch (Exception exception) {
+      System.err.println(exception.toString());
+    } finally {
+      channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+    }
+  }
 
-    private Instant fromCustomTime(Timestamp time) {
-      return Instant.ofEpochSecond(time.getSeconds());
-    }
- 
-    private void info(String msg, Object... params) {
-      logger.log(Level.INFO, msg, params);
-    }
+  private Instant fromCustomTime(Timestamp time) {
+    return Instant.ofEpochSecond(time.getSeconds());
+  }
+
+  private void info(String msg, Object... params) {
+    logger.log(Level.INFO, msg, params);
+  }
 }
--- src/main/java/ds/mysmartgym/SmartGymServer.java
+++ src/main/java/ds/mysmartgym/SmartGymServer.java
@@ -1,82 +1,97 @@
 package ds.mysmartgym;
 
+import com.google.protobuf.Timestamp;
+import com.google.type.DateTime;
+import ds.mysmartgym.MySmartGymGrpc.MySmartGymImplBase;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 import java.time.Instant;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
-import com.google.protobuf.Timestamp;
-import com.google.type.DateTime;
-
-import java.util.*;
-
-import ds.mysmartgym.MySmartGymGrpc.MySmartGymImplBase;
-
 public class SmartGymServer {
-    private static final Logger logger = Logger.getLogger(SmartGymServer.class.getName());
+  private static final Logger logger =
+      Logger.getLogger(SmartGymServer.class.getName());
 
-    private Server server;
-  
-    private void start() throws IOException {
-      /* The port on which the server should run */
-      int port = 50051;
-      server = ServerBuilder.forPort(port)
-          .addService(new MySmartGymImpl())
-          .build()
-          .start();
-      logger.info("Server started, listening on " + port);
-      Runtime.getRuntime().addShutdownHook(new Thread() {
-        @Override
-        public void run() {
-          // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-          System.err.println("*** shutting down gRPC server since JVM is shutting down");
-          try {
-            SmartGymServer.this.stop();
-          } catch (InterruptedException e) {
-            e.printStackTrace(System.err);
-          }
-          System.err.println("*** server shut down");
+  private Server server;
+
+  private void start() throws IOException {
+    /* The port on which the server should run */
+    int port = 50051;
+    server = ServerBuilder.forPort(port)
+                 .addService(new MySmartGymImpl())
+                 .build()
+                 .start();
+    logger.info("Server started, listening on " + port);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        // Use stderr here since the logger may have been reset by its JVM
+        // shutdown hook.
+        System.err.println(
+            "*** shutting down gRPC server since JVM is shutting down");
+        try {
+          SmartGymServer.this.stop();
+        } catch (InterruptedException e) {
+          e.printStackTrace(System.err);
         }
-      });
-    }
-  
-    private void stop() throws InterruptedException {
-      if (server != null) {
-        server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        System.err.println("*** server shut down");
       }
-    }
-  
-    /**
-     * Await termination on the main thread since the grpc library uses daemon threads.
-     */
-    private void blockUntilShutdown() throws InterruptedException {
-      if (server != null) {
-        server.awaitTermination();
-      }
-    }
-    public static void main(String[] args) throws IOException, InterruptedException {
-        final SmartGymServer server = new SmartGymServer();
-        server.start();
-        server.blockUntilShutdown();
-      }
+    });
+  }
 
-      static class MySmartGymImpl extends MySmartGymImplBase {
-        List<SavedWeight> savedWeights = new ArrayList<SavedWeight>();
+  private void stop() throws InterruptedException {
+    if (server != null) {
+      server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+    }
+  }
 
-        @Override
-        public void weightUpdate(Weight request, StreamObserver<Empty> responseObserver) {
-            SavedWeight savedWeight = SavedWeight.newBuilder().setWeight(request).setTime(currentTime()).build();
-            savedWeights.add(savedWeight);
-        }
-        
-      }
+  /**
+   * Await termination on the main thread since the grpc library uses daemon
+   * threads.
+   */
+  private void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+  public static void main(String[] args)
+      throws IOException, InterruptedException {
+    final SmartGymServer server = new SmartGymServer();
+    server.start();
+    server.blockUntilShutdown();
+  }
 
-      private static Timestamp currentTime() {
-        Instant time = Instant.now();
-        return Timestamp.newBuilder().setSeconds(time.getEpochSecond()).build();
-      }
-      
+  static class MySmartGymImpl extends MySmartGymImplBase {
+    List<SavedWeight> savedWeights = new ArrayList<SavedWeight>();
+
+    @Override
+    public void weightUpdate(Weight request,
+                             StreamObserver<Empty> responseObserver) {
+      SavedWeight savedWeight = SavedWeight.newBuilder()
+                                    .setWeight(request)
+                                    .setTime(currentTime())
+                                    .build();
+      savedWeights.add(savedWeight);
+      Empty reply = Empty.newBuilder().build();
+      responseObserver.onNext(reply);
+      responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getSavedWeights(Empty request,
+                                StreamObserver<SavedWeight> responseObserver) {
+      System.err.println("Saved weights");
+      savedWeights.forEach(responseObserver::onNext);
+      responseObserver.onCompleted();
+    }
+  }
+
+  private static Timestamp currentTime() {
+    Instant time = Instant.now();
+    return Timestamp.newBuilder().setSeconds(time.getEpochSecond()).build();
+  }
 }
